d4b5f01aa6e1406ce2a6fe75019b0047827e9149
viniciusdev00 <vinicius.biancolini.ts24@gmail.com>
2025-12-29 00:37:22 -0300

    upd1

---
+++ BACK/src/main/java/com/store/BACK/model/StatusPedido.java
@@ -0,0 +1,12 @@
+package com.store.BACK.model;
+
+public class StatusPedido {
+    public static final String PENDENTE = "PENDENTE";
+    public static final String PAGO = "PAGO";
+    public static final String ENVIADO = "ENVIADO";
+    public static final String ENTREGUE = "ENTREGUE";
+    public static final String CANCELADO = "CANCELADO";
+    
+    // Construtor privado para evitar instância
+    private StatusPedido() {}
+}
