5562a971a87743510eadd4dbcbf0126820eb21f0
naoeojapa <japauniversestore@gmail.com>
2026-02-12 09:13:54 -0300

    create

--- BACK/src/main/resources/data.sql
+++ BACK/src/main/resources/data.sql
@@ -3,15 +3,6 @@
 -- VERSÃO FINAL ATUALIZADA COM PREÇOS DA LISTA E IMAGENS .WEBP
 -- ==================================================================================
 
--- 1. LIMPEZA DE DADOS
-DELETE FROM itens_pedido;
-DELETE FROM pagamentos;
-DELETE FROM pedido_aviso;
-DELETE FROM pedidos;
-DELETE FROM produtos;
-DELETE FROM categorias;
-DELETE FROM marcas;
-
 -- 2. INSERÇÃO DE MARCAS
 INSERT INTO marcas (id, nome) VALUES 
 (1, 'Nike'), (2, 'Air Jordan'), (3, 'Adidas'), (4, 'Bape'), (5, 'Asics'),
