73d9819f8b38835351fb65b459efdc157758b236
naoeojapa <japauniversestore@gmail.com>
2026-03-19 16:35:33 -0300

    preco nocta

--- BACK/src/main/resources/data.sql
+++ BACK/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- Inserir um novo cupom de desconto
 INSERT IGNORE INTO cupons (id, codigo, desconto, tipo_desconto, data_validade) 
-VALUES (4, 'GUI10', 10.00, 'PERCENTUAL', '2030-12-31');
+VALUES (5, 'WS10', 10.00, 'PERCENTUAL', '2030-12-31');
